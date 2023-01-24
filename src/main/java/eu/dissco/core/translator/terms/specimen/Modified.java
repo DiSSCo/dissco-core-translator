47521f339fe61830d02489d46043a971ca394860
Sam Leeflang <sam.leeflang@naturalis.nl>
2023-01-24 16:10:54 +0100

    Unused import

--- src/main/java/eu/dissco/core/translator/terms/specimen/Modified.java
+++ src/main/java/eu/dissco/core/translator/terms/specimen/Modified.java
@@ -2,12 +2,12 @@ package eu.dissco.core.translator.terms.specimen;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import eu.dissco.core.translator.terms.Term;
-import java.time.Instant;
 import java.util.List;
 import org.gbif.dwc.ArchiveFile;
 import org.gbif.dwc.record.Record;
 
 public class Modified extends Term {
+
   public static final String TERM = ODS_PREFIX + "modified";
   private final List<String> dwcaTerms = List.of("dcterms:modified");
   private final List<String> abcdTerms = List.of("abcd:hasDateModified", "abcd:dateLastEdited");
