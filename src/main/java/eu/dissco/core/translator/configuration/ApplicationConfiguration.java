b45406f946f2edf33bb1c32e8d7a11f4bbf1c3c9
Sam Leeflang <sam.leeflang@naturalis.nl>
2023-04-25 09:21:27 +0200

    Fix timestamp and NP in License

---
+++ src/main/java/eu/dissco/core/translator/configuration/ApplicationConfiguration.java
@@ -0,0 +1,24 @@
+package eu.dissco.core.translator.configuration;
+
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import java.time.Instant;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApplicationConfiguration {
+  public static final String DATE_STRING = "yyyy-MM-dd'T'HH:mm:ss.SSSXXX";
+
+  @Bean
+  public ObjectMapper objectMapper() {
+    var mapper = new ObjectMapper().findAndRegisterModules();
+    SimpleModule dateModule = new SimpleModule();
+    dateModule.addSerializer(Instant.class, new InstantSerializer());
+    dateModule.addDeserializer(Instant.class, new InstantDeserializer());
+    mapper.registerModule(dateModule);
+    mapper.setSerializationInclusion(Include.NON_NULL);
+    return mapper;
+  }
+}
