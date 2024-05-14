2f42b064e225ba4e48ea1cceb737573e9d439192
Sam Leeflang <sam.leeflang@naturalis.nl>
2024-05-14 13:22:08 +0200

    Add ABCD taxon rank

--- src/main/java/eu/dissco/core/translator/terms/specimen/identification/taxonomy/TaxonRank.java
+++ src/main/java/eu/dissco/core/translator/terms/specimen/identification/taxonomy/TaxonRank.java
@@ -8,10 +8,17 @@ public class TaxonRank extends AbstractTaxonomy {
   public static final String TERM = DWC_PREFIX + "taxonRank";
 
   private final List<String> dwcaTerms = List.of(TERM, VerbatimTaxonRank.TERM);
+  private final List<String> abcdTerms = List.of(
+      "result/taxonIdentified/scientificName/nameAtomised/botanical/rank");
 
   @Override
   public String retrieveFromDWCA(JsonNode unit) {
     return super.searchJsonForTerm(unit, dwcaTerms);
+  }
+  
+  @Override
+  public String retrieveFromABCD(JsonNode unit) {
+    return searchJsonForTerm(unit, abcdTerms);
   }
 
   @Override
