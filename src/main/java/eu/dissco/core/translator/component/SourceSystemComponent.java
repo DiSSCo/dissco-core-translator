3809282e5f2267aa478872920aa0f43499b8ed7c
Sam Leeflang <sam.leeflang@naturalis.nl>
2024-09-02 13:49:18 +0200

    Fix Sonar review

--- src/main/java/eu/dissco/core/translator/component/SourceSystemComponent.java
+++ src/main/java/eu/dissco/core/translator/component/SourceSystemComponent.java
@@ -5,24 +5,16 @@ import eu.dissco.core.translator.repository.SourceSystemRepository;
 import lombok.Getter;
 import org.springframework.stereotype.Component;
 
-
+@Getter
 @Component
 public class SourceSystemComponent {
 
-  private final WebClientProperties webClientProperties;
-  private final SourceSystemRepository repository;
-  @Getter
   private final String sourceSystemID;
-  @Getter
   private final String sourceSystemName;
-  @Getter
   private final String sourceSystemEndpoint;
 
   public SourceSystemComponent(WebClientProperties webClientProperties,
       SourceSystemRepository repository) {
-    this.webClientProperties = webClientProperties;
-    this.repository = repository;
-
     this.sourceSystemID = webClientProperties.getSourceSystemId();
     var sourceSystemInformation = repository.getSourceSystem(this.sourceSystemID);
     if (sourceSystemInformation == null) {
