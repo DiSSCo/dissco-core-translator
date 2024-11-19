ed287b443fb46f8e21e82302c35bcb8282cabfec
Sam Leeflang <sam.leeflang@naturalis.nl>
2024-11-19 15:57:59 +0100

    Add pattern match for UUID

--- src/main/java/eu/dissco/core/translator/terms/utils/IdentifierUtils.java
+++ src/main/java/eu/dissco/core/translator/terms/utils/IdentifierUtils.java
@@ -25,13 +25,13 @@ import org.apache.commons.lang3.tuple.Triple;
 @Slf4j
 public class IdentifierUtils {
 
-  private static final Map<List<Pattern>, Triple<DctermsType, String, OdsGupriLevel>> map = getPrefixMap();
+  private static final Map<List<Pattern>, Triple<DctermsType, String, OdsGupriLevel>> PATTERN_MAP = patternMap();
 
   private IdentifierUtils() {
     // This is a Utility class
   }
 
-  private static Map<List<Pattern>, Triple<DctermsType, String, OdsGupriLevel>> getPrefixMap() {
+  private static Map<List<Pattern>, Triple<DctermsType, String, OdsGupriLevel>> patternMap() {
     var linkedMap = new LinkedHashMap<List<Pattern>, Triple<DctermsType, String, OdsGupriLevel>>();
     linkedMap.put(List.of(compile("^https?://doi.org")),
         Triple.of(DOI, "DOI", GLOBALLY_UNIQUE_STABLE_PERSISTENT_RESOLVABLE_FDO_COMPLIANT));
@@ -41,15 +41,14 @@ public class IdentifierUtils {
         Triple.of(URL, "Wikidata", GLOBALLY_UNIQUE_STABLE_PERSISTENT_RESOLVABLE));
     linkedMap.put(List.of(compile("^https?://orcid.org")),
         Triple.of(URL, "ORCID", GLOBALLY_UNIQUE_STABLE_PERSISTENT_RESOLVABLE));
-    linkedMap.put(List.of(compile("^https?://orcid.org")),
-        Triple.of(URL, "ORCID", GLOBALLY_UNIQUE_STABLE_PERSISTENT_RESOLVABLE));
     linkedMap.put(List.of(compile("^https?://\\w+.\\w+/ark:/\\w+/\\w+")),
         Triple.of(ARK, "ARK", GLOBALLY_UNIQUE_STABLE_PERSISTENT_RESOLVABLE));
     linkedMap.put(List.of(compile("https?://purl.org")),
         Triple.of(PURL, "PURL", GLOBALLY_UNIQUE_STABLE_PERSISTENT_RESOLVABLE));
     linkedMap.put(List.of(compile("^https?")),
         Triple.of(URL, "URL", GLOBALLY_UNIQUE_STABLE_PERSISTENT_RESOLVABLE));
-    linkedMap.put(List.of(compile("^urn:uuid")),
+    linkedMap.put(List.of(compile(
+            "(uuid:)*[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}")),
         Triple.of(DctermsType.UUID, "UUID", GLOBALLY_UNIQUE_STABLE));
     return linkedMap;
   }
@@ -72,7 +71,7 @@ public class IdentifierUtils {
         .withType("ods:Identifier")
         .withDctermsIdentifier(identifierString)
         .withOdsIdentifierStatus(identifierStatus);
-    for (var entry : map.entrySet()) {
+    for (var entry : PATTERN_MAP.entrySet()) {
       for (var prefix : entry.getKey()) {
         if (prefix.matcher(identifierString).find()) {
           identifier.setDctermsType(entry.getValue().getLeft());
@@ -82,20 +81,13 @@ public class IdentifierUtils {
         }
       }
     }
-    if (isValidUUID(identifierString)) {
-      identifier.setDctermsType(DctermsType.UUID);
-      identifier.setDctermsTitle(getDcTermsTitle(identifierName, "UUID"));
-      identifier.setOdsGupriLevel(GLOBALLY_UNIQUE_STABLE);
-      return identifier;
-    } else {
-      log.debug(
-          "Unable to recognise the type of identifier: {}. Assuming locally unique identifier",
-          identifierString);
-      identifier.setDctermsType(DctermsType.LOCALLY_UNIQUE_IDENTIFIER);
-      identifier.setDctermsTitle(identifierName);
-      identifier.setOdsGupriLevel(OdsGupriLevel.LOCALLY_UNIQUE_STABLE);
-      return identifier;
-    }
+    log.debug(
+        "Unable to recognise the type of identifier: {}. Assuming locally unique identifier",
+        identifierString);
+    identifier.setDctermsType(DctermsType.LOCALLY_UNIQUE_IDENTIFIER);
+    identifier.setDctermsTitle(identifierName);
+    identifier.setOdsGupriLevel(OdsGupriLevel.LOCALLY_UNIQUE_STABLE);
+    return identifier;
   }
 
   private static String getDcTermsTitle(String identifierName, String defaultValue) {
