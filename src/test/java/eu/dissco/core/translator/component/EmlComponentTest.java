fd6092a5bd43596f9335b47630658ca78e9d5485
Sam Leeflang <sam.leeflang@naturalis.nl>
2025-06-25 11:32:48 +0200

    Code review

--- src/test/java/eu/dissco/core/translator/component/EmlComponentTest.java
+++ src/test/java/eu/dissco/core/translator/component/EmlComponentTest.java
@@ -1,6 +1,9 @@
 package eu.dissco.core.translator.component;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mockStatic;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.when;
 
 import efg.ContentMetadata;
 import efg.ContentMetadata.Description;
@@ -13,32 +16,32 @@ import efg.MetadataDescriptionRepr;
 import efg.MicroAgentP;
 import efg.Statement;
 import java.io.IOException;
+import java.time.Clock;
 import java.time.Instant;
+import java.util.UUID;
 import java.util.stream.Stream;
 import javax.xml.datatype.DatatypeConfigurationException;
 import javax.xml.datatype.DatatypeFactory;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.MockedStatic;
 
 class EmlComponentTest {
 
-
-  @MethodSource("generateDataset")
-  @ParameterizedTest
-  void testGenerateEml(DataSets.DataSet dataset, String expected) throws IOException {
-    // When
-    var result = EmlComponent.generateEML(dataset);
-
-    // Then
-    assertThat(result).contains(expected);
-  }
+  private MockedStatic<Clock> clockMock;
+  private MockedStatic<UUID> uuidMock;
 
   private static Stream<Arguments> generateDataset() throws DatatypeConfigurationException {
     return Stream.of(
-        Arguments.of(generateFullDataset(), "This is a test dataset"),
-        Arguments.of(generateDataSetWithEmptyLists(), "Dataset with empty lists"),
-        Arguments.of(generateMinimalDataset(), "Minimal Dataset")
+        Arguments.of(generateFullDataset(),
+            "<eml xmlns=\"https://eml.ecoinformatics.org/eml-2.2.0\" packageId=\"123e4567-e89b-12d3-a456-426614174000\" system=\"https://dissco.eu\" xml:lang=\"en\"><dataset xmlns=\"\"><title>Dataflos</title><pubDate>2025-06-25</pubDate><language>en</language><abstract>This is a test dataset</abstract><contact><individualName><surname>Sam</surname></individualName><address><deliveryPoint>123 Test Street</deliveryPoint></address><electronicMailAddress>test@test.com</electronicMailAddress><phone>+123456789</phone></contact><associatedParty><individualName><surname>Sam</surname></individualName><role>TECHNICAL_POINT_OF_CONTACT</role></associatedParty><intellectualRights>Only be used for testing purposes</intellectualRights><licensed><licenseName>CC-BY-4.0</licenseName><url>https://creativecommons.org/licenses/by/4.0/</url></licensed><distribution><online><url>http://example.com/dataset</url></online></distribution><coverage><geographicCoverage><geographicDescription>Global</geographicDescription><boundingCoordinates><westBoundingCoordinate>-180</westBoundingCoordinate><eastBoundingCoordinate>180</eastBoundingCoordinate><northBoundingCoordinate>-90</northBoundingCoordinate><southBoundingCoordinate>90</southBoundingCoordinate></boundingCoordinates></geographicCoverage></coverage></dataset></eml>"),
+        Arguments.of(generateDataSetWithEmptyLists(),
+            "<eml xmlns=\"https://eml.ecoinformatics.org/eml-2.2.0\" packageId=\"123e4567-e89b-12d3-a456-426614174000\" system=\"https://dissco.eu\" xml:lang=\"en\"><dataset xmlns=\"\"><title>Dataset with empty lists</title></dataset></eml>"),
+        Arguments.of(generateMinimalDataset(),
+            "<eml xmlns=\"https://eml.ecoinformatics.org/eml-2.2.0\" packageId=\"123e4567-e89b-12d3-a456-426614174000\" system=\"https://dissco.eu\" xml:lang=\"en\"><dataset xmlns=\"\"><title>Minimal Dataset</title><intellectualRights>Only be used for testing purposes</intellectualRights></dataset></eml>")
     );
   }
 
@@ -68,7 +71,7 @@ class EmlComponentTest {
     technicalContacts.getTechnicalContact();
     dataset.setTechnicalContacts(technicalContacts);
     dataset.setContentContacts(contentContacts);
-    return  dataset;
+    return dataset;
   }
 
   private static DataSets.DataSet generateMinimalDataset() {
@@ -87,7 +90,7 @@ class EmlComponentTest {
     termsOfUseStatements.getTermsOfUse().add(statement);
     iprStatements.setTermsOfUseStatements(termsOfUseStatements);
     metadata.setIPRStatements(iprStatements);
-    return  dataset;
+    return dataset;
   }
 
   private static DataSets.DataSet generateFullDataset() throws DatatypeConfigurationException {
@@ -135,6 +138,43 @@ class EmlComponentTest {
     dataset.setTechnicalContacts(technicalContacts);
     dataset.setContentContacts(contentContacts);
     return dataset;
+  }
+
+  @BeforeEach
+  void setup() {
+    mockInstant(1750843566); // set desired return value 2021-12-20T11:33:20Z
+    mockUUID();
+  }
+
+  @AfterEach
+  void destroy() {
+    clockMock.close();
+    uuidMock.close();
+  }
+
+  @MethodSource("generateDataset")
+  @ParameterizedTest
+  void testGenerateEml(DataSets.DataSet dataset, String expected) throws IOException {
+    // Given
+
+    // When
+    var result = EmlComponent.generateEML(dataset);
+
+    // Then
+    assertThat(result).isEqualTo(expected);
+  }
+
+  private void mockInstant(long expected) {
+    Clock spyClock = spy(Clock.systemDefaultZone());
+    clockMock = mockStatic(Clock.class);
+    clockMock.when(Clock::systemDefaultZone).thenReturn(spyClock);
+    when(spyClock.instant()).thenReturn(Instant.ofEpochSecond(expected));
+  }
+
+  private void mockUUID() {
+    var uuid = UUID.fromString("123e4567-e89b-12d3-a456-426614174000");
+    uuidMock = mockStatic(UUID.class);
+    uuidMock.when(UUID::randomUUID).thenReturn(uuid);
   }
 
 }
