436e81c3c3473dfd5d44995154f248cc917eab22
Sam Leeflang <sam.leeflang@naturalis.nl>
2026-02-23 10:51:38 +0100

    Update Dockerfile to java 25

--- src/main/java/eu/dissco/core/translator/domain/DigitalMediaEvent.java
+++ src/main/java/eu/dissco/core/translator/domain/DigitalMediaEvent.java
@@ -4,6 +4,7 @@ import java.util.Set;
 
 public record DigitalMediaEvent(
     Set<String> masList,
-    DigitalMediaWrapper digitalMedia, Boolean forceMasSchedule) {
+    DigitalMediaWrapper digitalMedia,
+    Boolean forceMasSchedule) {
 
 }
